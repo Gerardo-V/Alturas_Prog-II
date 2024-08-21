e7d75095983ae7f40d3142a14b2fc7536fc4ceeb
Gerardo <gerardovillarraga1019@gmail.com>
2024-08-21 08:00:42 -0500

    se agrego un brebe explicacion del funcionamiento a lo largo del codigo

--- src/alturas/Alturas.java
+++ src/alturas/Alturas.java
@@ -16,21 +16,23 @@ public class Alturas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc = new Scanner (System.in);
-        Cal_altura objt = new Cal_altura ();
+        Scanner sc = new Scanner (System.in);       // se definen las variables, vector y declaramos nuestra otras clase como 
+        Cal_altura objt = new Cal_altura ();        // objeto para poder usarla 
         double Vec_altur[] = new double [25];
-        
-        for(int i=0; i<25; i++){
+ 
+
+        //                                          // usamos un for para que el usuario pueda ingresar en el vector con todas las 
+        for(int i=0; i<25; i++){                    // alturas que desea comprovar 
         System.out.println ("ingrese la altura numero "+(i+1)+" (ej. 1,68)");
         Vec_altur[i]= sc.nextDouble();
         }
         
-        objt.vec_Altura(Vec_altur);
+        objt.vec_Altura(Vec_altur);                 // llamamos/establecemos las funciones o partes que usaremos de nuestro objeto 
         objt.Cal_altur();
         
-        System.out.println("");
-        System.out.println("la mayor altura ingresada fue de "+objt.getAltur_max());
-        System.out.println("la menor altura ingresada fue de "+objt.getAltur_min());
+        System.out.println("");                     // llamamos nuestras respuestas para mostrarlas en pantalla 
+        System.out.println("la mayor altura ingresada fue de "+objt.getAltur_max()+"m");
+        System.out.println("la menor altura ingresada fue de "+objt.getAltur_min()+"m");
         System.out.println("");
         
     }
--- src/alturas/Cal_altura.java
+++ src/alturas/Cal_altura.java
@@ -8,19 +8,19 @@ package alturas;
  *
  * @author ESTUDIANTE
  */
-public class Cal_altura {
+public class Cal_altura {                           // establecemos todos las variables que nesesitaremos 
     private double Altur[], Altur_max, Altur_min;
     
     public Cal_altura(){
     }
     
-    public void vec_Altura (double Altura[]){
+    public void vec_Altura (double Altura[]){       // llamamos nuestro vector 
         this.Altur = Altura;
     }
     
-    public void Cal_altur (){
-        int i;
-        Altur_min=100.00;
+    public void Cal_altur (){                       // usando un ciclo for con 2 if en su interior para comprovar
+        int i;                                      // y guardar las alturas que nesesitamos para el software dentro
+        Altur_min=100.00;                           // de las variables creadas
         for (i=0; i<Altur.length; i++){
             if (Altur[i]>Altur_max){
                 Altur_max=Altur[i];
@@ -31,8 +31,8 @@ public class Cal_altura {
         }
     }
     
-    public double getAltur_max(){
-        return Altur_max;
+    public double getAltur_max(){                   //permitimos retornar los resultados que fueron almacenados para 
+        return Altur_max;                           //que despues puedan ser mostrados en Alturas (main)
     }
     public double getAltur_min(){
         return Altur_min;
